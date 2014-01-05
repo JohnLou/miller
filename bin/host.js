e66eec95dfd666804317a62da1486765e4ff9601
John Lou <new.flar@gmail.com>
2014-01-05 17:18:53 +0800

    temp1

--- bin/host.js
+++ bin/host.js
@@ -1,22 +1,12 @@
 #!/usr/bin/env node
 var socketRedis = require('../socket-redis.js');
 var childProcess = require('child_process');
-var utils = require('../lib/utils.js');
+
 var optimist = require('optimist').default('log-dir', null);
 var fs = require('fs');
 var argv = optimist.default('redis-host', 'localhost').argv;
 var redisHost = argv['redis-host'];
-var logDir = argv['log-dir'];
-var sockjsClientUrl = argv['sockjs-client-url'];
-var sslKey = argv['ssl-key'];
-var sslCert = argv['ssl-cert'];
-var sslPfx = argv['ssl-pfx'];
-var sslPassphrase = argv['ssl-passphrase'];
 
-
-if (logDir) {
-    utils.logProcessInto(process, logDir + '/miller-server.log');
-}
 
 if (!process.send) {
     argv = optimist.default('socket-ports', '9801').default('status-port', '9810').argv;
@@ -25,24 +15,7 @@ if (!process.send) {
 
     socketPorts.forEach(function(socketPort) {
         var args = ['--socket-port=' + socketPort];
-        if (logDir) {
-            args.push('--log-dir=' + logDir);
-        }
-        if (sockjsClientUrl) {
-            args.push('--sockjs-client-url=' + sockjsClientUrl);
-        }
-        if (sslKey) {
-            args.push('--ssl-key=' + sslKey);
-        }
-        if (sslCert) {
-            args.push('--ssl-cert=' + sslCert);
-        }
-        if (sslPfx) {
-            args.push('--ssl-pfx=' + sslPfx);
-        }
-        if (sslPassphrase) {
-            args.push('--ssl-passphrase=' + sslPassphrase);
-        }
+
         var startWorker = function() {
             var worker = childProcess.fork(__filename, args);
             console.log('Starting worker `' + worker.pid + '` to listen on port `' + socketPort + '`');
@@ -64,35 +37,4 @@ if (!process.send) {
         process.exit();
     });
 
-} else {
-    var sslOptions = null;
-    if (sslKey && sslCert) {
-        sslOptions = {
-            key: fs.readFileSync(sslKey)
-        };
-
-        var certFile = fs.readFileSync(sslCert).toString();
-        var certs = certFile.match(/(-+BEGIN CERTIFICATE-+[\s\S]+?-+END CERTIFICATE-+)/g);
-        if (certs && certs.length) {
-            sslOptions.cert = certs.shift();
-            if (certs.length) {
-                sslOptions.ca = certs;
-            }
-        } else {
-            sslOptions.cert = certFile;
-        }
-    }
-    if (sslPfx) {
-        sslOptions = {
-            pfx: fs.readFileSync(sslPfx)
-        };
-    }
-    if (sslOptions && sslPassphrase) {
-        sslOptions.passphrase = fs.readFileSync(sslPassphrase).toString().trim();
-    }
-    var socketPort = argv['socket-port'];
-    var worker = new socketRedis.Worker(socketPort, sockjsClientUrl, sslOptions);
-    process.on('message', function(event) {
-        worker.triggerEventDown(event.type, event.data);
-    });
 }
